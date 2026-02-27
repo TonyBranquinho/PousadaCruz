1c47f29312cd28e831ccf9a84a1c1037f0d93449
Tony <antony.branquinho@gmail.com>
2026-02-27 14:20:28 -0300

    inclui v=1.0 para forçar os navegadores a recarregar

---
+++ script.js
@@ -0,0 +1,7 @@
+
+{/* Espera 5 segundos (5000 milissegundos) antes de mostrar o botão */}
+setTimeout(function() {
+    const botaoZap = document.querySelector('.btn-whatsapp');
+    botaoZap.classList.add('mostrar');
+}, 5000); 
+  
